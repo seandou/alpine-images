08a4cc9b158355739965a23409d4246905b60b0c
Sean Dou <douguohui@gmail.com>
2016-10-25 18:35:25 +0800

    set node version

--- nodejs/build-node.sh
+++ nodejs/build-node.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+NODE_VERSION="v4.6.1"
+
+NODE_PACKAGE="node-$NODE_VERSION.tar.gz"
+TMP_FILE="/tmp/$NODE_PACKAGE"
+TMP_DIR="/tmp/node-$NODE_VERSION"
+
 # Install building tools
 apk add --no-cache curl make gcc g++ python linux-headers paxctl libgcc libstdc++ gnupg
 
 # Download and unpack node package
-wget -c https://nodejs.org/dist/v4.6.1/node-v4.6.1.tar.gz -O /tmp/node-v4.6.1.tar.gz
-cd /tmp/ && tar xzf node-v4.6.1.tar.gz
+wget -c https://nodejs.org/dist/$NODE_VERSION/$NODE_PACKAGE -O $TMP_FILE
+cd /tmp/ && tar xzf $NODE_PACKAGE
 
 # Building
-cd /tmp/node-v4.6.1/ && ./configure && make && make install
+cd $TMP_DIR && ./configure && make && make install
 
 # Clean up temp files
-rm -rf /tmp/node-v4.6.1.tar.gz /tmp/node-v4.6.1
+rm -rf $TMP_FILE $TMP_DIR
 
 # Update npm and install cnpm
 npm install -g npm cnpm
