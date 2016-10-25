98e63e241340e0def79c63b6e7f7c5436e088f9a
Sean Dou <douguohui@gmail.com>
2016-10-25 18:10:11 +0800

    nodejs build script

---
+++ nodejs/build-node.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Install building tools
+apk add --no-cache curl make gcc g++ python linux-headers paxctl libgcc libstdc++ gnupg
+
+# Download and unpack node package
+wget -c https://nodejs.org/dist/v4.6.1/node-v4.6.1.tar.gz /tmp/node-v4.6.1.tar.gz
+cd /tmp/ && tar xzf node-v4.6.1.tar.gz
+
+# Building
+cd /tmp/node-v4.6.1/ && ./configure && make && make install
+
+# Update npm and install cnpm
+npm install -g npm cnpm
