50dc991ffa98a177c6a55d712596f4f4d730dd60
Sean Dou <douguohui@gmail.com>
2016-10-26 16:51:36 +0800

    building chain

--- nodejs/build-node.sh
+++ nodejs/build-node.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 NODE_VERSION="v4.6.1"
 
@@ -10,16 +10,17 @@ TMP_DIR="/tmp/node-$NODE_VERSION"
 apk add --no-cache curl make gcc g++ python linux-headers paxctl libgcc libstdc++ gnupg
 
 # Download and unpack node package
-wget -c https://nodejs.org/dist/$NODE_VERSION/$NODE_PACKAGE -O $TMP_FILE
+wget -c https://npm.taobao.org/mirrors/node/$NODE_VERSION/$NODE_PACKAGE -O $TMP_FILE
 cd /tmp/ && tar xzf $NODE_PACKAGE
 
 # Building
-cd $TMP_DIR && ./configure && make && make install
+cd $TMP_DIR \
+  && ./configure \
+  && make \
+  && make install \
+  && npm install -g cnpm --registry=https://registry.npm.taobao.org \
+  && cnpm install -g npm \
+  && cnpm install -g yarn
 
 # Clean up temp files
 rm -rf $TMP_FILE $TMP_DIR
-
-# Install cnpm、yarn and update npm
-npm install -g cnpm
-npm install -g yarn
-cnpm install -g npm
